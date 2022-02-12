1c814e5478d6dd5b64d882b4e0d9d69458abf7d5
jenbrissman <brissman514@gmail.com>
2022-02-11 21:51:55 -0500

    updating code with new file name

--- upload-and-transform-asset.js
+++ upload-and-transform-asset.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.uploader.upload('./assets/woman.jpg', {
-  public_id: 'woman',
+cloudinary.uploader.upload('./assets/smiling.jpg', {
+  public_id: 'smiling',
   upload_preset: 'profile-preset'
 })
 .then(uploadResult => console.log(uploadResult))
